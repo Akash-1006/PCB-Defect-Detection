49a6827d38f012f2070ddfc5724a8627750c6488
Akash Balaji <113533511+Akash-1006@users.noreply.github.com>
2025-09-27 22:08:58 +0530

    Everything over bro

--- app.py
+++ app.py
@@ -265,4 +265,4 @@ def run_rule_based_aoi(test_path: str, output_dir: str) -> str:
 
 # ---------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
